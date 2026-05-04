766b97325b1437a9952eafe44cbe8d462f670bcd
mariampervez <mariam.pervez@nav.no>
2026-05-04 13:40:06 +0200

    F!- Legger til vitale filer for å få opp applikasjonen :)
    Co-authored-by: Daniel.skarpas@nav.no

---
+++ src/main/kotlin/EessiDodsmeldingApplication.kt
@@ -0,0 +1,15 @@
+package no.nav.eessi.pensjon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+
+class EessiDodsmeldingApplication
+fun main() {
+
+    fun main(args: Array<String>) {
+        runApplication<EessiDodsmeldingApplication>(*args)
+    }
+
+}
