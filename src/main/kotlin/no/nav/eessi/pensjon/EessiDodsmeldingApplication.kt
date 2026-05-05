71af9fad80e2ffcc8cfc72c408ba8524812c0fc9
Daniel Skarpås <dskarpas@gmail.com>
2026-05-05 12:57:17 +0200

    F - Legger tilbake auth

--- src/main/kotlin/no/nav/eessi/pensjon/EessiDodsmeldingApplication.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/EessiDodsmeldingApplication.kt
@@ -1,4 +1,4 @@
-package no.nav.eessi.pensjon.no.nav.eessi.pensjon
+package no.nav.eessi.pensjon
 
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Profile
 
-//@EnableJwtTokenValidation(ignore = ["org.springframework", "no.nav.eessi.pensjon.shared.api.health.DiagnosticsController"])
-//@EnableOAuth2Client(cacheEnabled = true)
+@EnableJwtTokenValidation(ignore = ["org.springframework", "no.nav.eessi.pensjon.shared.api.health.DiagnosticsController"])
+@EnableOAuth2Client(cacheEnabled = true)
 @SpringBootApplication
 @Profile("!unsecured-webmvctest")
 class EessiDodsmeldingApplication
