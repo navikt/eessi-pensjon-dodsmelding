157af9a6e82a4255a3833612335e3fad5d6d6c72
Daniel Skarpås <dskarpas@gmail.com>
2026-05-05 09:27:17 +0200

    F - Fjerner litt config, legger til EnableOAuth2Client,  EnableJwtTokenValidation
    Co-authored-by: mariam pervez <mariam.pervez@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/EessiDodsmeldingApplication.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/EessiDodsmeldingApplication.kt
@@ -1,11 +1,17 @@
 package no.nav.eessi.pensjon.no.nav.eessi.pensjon
 
+import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Profile
 
+@EnableJwtTokenValidation(ignore = ["org.springframework", "no.nav.eessi.pensjon.shared.api.health.DiagnosticsController"])
+@EnableOAuth2Client(cacheEnabled = true)
 @SpringBootApplication
-
+@Profile("!unsecured-webmvctest")
 class EessiDodsmeldingApplication
-    fun main(args: Array<String>) {
-        runApplication<EessiDodsmeldingApplication>(*args)
-    }
+
+fun main(args: Array<String>) {
+    runApplication<EessiDodsmeldingApplication>(*args)
+}
