646f7fbcf17a0c3b18ae5c4e4047bdd646a56385
Daniel Skarpås <dskarpas@gmail.com>
2026-05-12 15:16:22 +0200

    F - Legger til service for PesysKlient.kt

--- src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/PesysKlient.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/PesysKlient.kt
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import kotlin.collections.map
 
-//@Component
+@Component
 class PesysKlient(private val pesysClientRestTemplate: RestTemplate) {
 
     private val logger: Logger by lazy { LoggerFactory.getLogger(PesysKlient::class.java) }
