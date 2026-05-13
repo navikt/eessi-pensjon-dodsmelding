34a714f9e7a3059ab08ce423ff410d445a12547f
Daniel Skarpås <dskarpas@gmail.com>
2026-05-13 13:41:42 +0200

    E - Legger inn mer kafka config
    Co-authored-by: mariam pervez <mariam.pervez@nav.no>

---
+++ src/main/kotlin/no/nav/eessi/pensjon/config/KafkaConfig.kt
@@ -0,0 +1,101 @@
+package no.nav.eessi.pensjon.config
+
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import no.nav.eessi.pensjon.oppgaverouting.logger
+import no.nav.person.pdl.leesah.Personhendelse
+import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.config.SslConfigs
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.*
+import org.springframework.kafka.listener.ContainerProperties
+import org.springframework.kafka.listener.DefaultErrorHandler
+import org.springframework.util.backoff.FixedBackOff
+import java.time.Duration
+
+@EnableKafka
+@Profile("prod", "test")
+@Configuration
+class KafkaConfig(
+    @param:Value("\${kafka.keystore.path}") private val keystorePath: String,
+    @param:Value("\${kafka.credstore.password}") private val credstorePassword: String,
+    @param:Value("\${kafka.truststore.path}") private val truststorePath: String,
+    @param:Value("\${kafka.brokers}") private val bootstrapServers: String,
+    @param:Value("\${kafka.security.protocol}") private val securityProtocol: String,
+    @Autowired private val kafkaErrorHandler: KafkaStoppingErrorHandler?
+) {
+
+//    @Bean
+//    fun producerFactory(): ProducerFactory<String, String> {
+//        val configMap: MutableMap<String, Any> = HashMap()
+//        populerCommonConfig(configMap)
+//        configMap[ProducerConfig.CLIENT_ID_CONFIG] = "eessi-pensjon-pdl-produsent"
+//        configMap[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+//        configMap[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+//        configMap[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
+//        return DefaultKafkaProducerFactory(configMap)
+//    }
+
+    @Bean
+    fun kafkaAivenHendelseListenerAvroLatestContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, Personhendelse> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, Personhendelse>()
+        factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL
+        factory.containerProperties.setAuthExceptionRetryInterval(Duration.ofSeconds(2))
+
+        val configMap: MutableMap<String, Any> = consumerConfigsLatestAvro()
+        populerCommonConfig(configMap)
+
+        factory.setConsumerFactory(
+            DefaultKafkaConsumerFactory(
+                configMap
+            )
+        )
+        factory.setCommonErrorHandler(kafkaRestartingErrorHandler())
+        return factory
+    }
+
+    fun kafkaRestartingErrorHandler(): DefaultErrorHandler {
+        return DefaultErrorHandler({ record, exception ->
+            logger.error("Kafka error, restarting container", exception)
+        }, FixedBackOff(5000, 3))
+    }
+
+    private fun consumerConfigsLatestAvro(): MutableMap<String, Any> {
+        val schemaRegisty = System.getenv("KAFKA_SCHEMA_REGISTRY") ?: throw RuntimeException("KAFKA_BROKERS må være satt i miljøet")
+        val schemaRegistryUser = System.getenv("KAFKA_SCHEMA_REGISTRY_USER") ?: throw RuntimeException("KAFKA_BROKERS må være satt i miljøet")
+        val schemaRegistryPassword = System.getenv("KAFKA_SCHEMA_REGISTRY_PASSWORD") ?: throw RuntimeException("KAFKA_BROKERS må være satt i miljøet")
+        logger.info("Setter opp consumer med følgende konfigurasjon: bootstrapServers=$bootstrapServers, schemaRegistry=$schemaRegisty, securityProtocol=$securityProtocol, schemaRegistryUser=$schemaRegistryUser")
+        val consumerConfigs =
+            mutableMapOf<String, Any>(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+                "schema.registry.url" to schemaRegisty,
+                "auth.exception.retry.interval" to "30s",
+                "basic.auth.credentials.source" to "USER_INFO",
+                "basic.auth.user.info" to "$schemaRegistryUser:$schemaRegistryPassword",
+                "specific.avro.reader" to "true",
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java.name,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to KafkaAvroDeserializer::class.java.name,
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
+            )
+        return consumerConfigs.also { logger.debug("Kafka consumer configs: {}", it) }
+    }
+    private fun populerCommonConfig(configMap: MutableMap<String, Any>) {
+        configMap[SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG] = keystorePath
+        configMap[SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG] = credstorePassword
+        configMap[SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG] = credstorePassword
+        configMap[SslConfigs.SSL_KEY_PASSWORD_CONFIG] = credstorePassword
+        configMap[SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG] = "JKS"
+        configMap[SslConfigs.SSL_KEYSTORE_TYPE_CONFIG] = "PKCS12"
+        configMap[SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG] = truststorePath
+        configMap[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = securityProtocol
+    }
+}
---
+++ src/main/kotlin/no/nav/eessi/pensjon/config/KafkaStoppingErrorHandler.kt
@@ -0,0 +1,35 @@
+package no.nav.eessi.pensjon.config
+
+import org.apache.kafka.clients.consumer.Consumer
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Profile
+import org.springframework.kafka.listener.CommonContainerStoppingErrorHandler
+import org.springframework.kafka.listener.MessageListenerContainer
+import org.springframework.stereotype.Component
+import java.lang.Exception
+
+
+@Profile("prod", "test")
+@Component
+class KafkaStoppingErrorHandler : CommonContainerStoppingErrorHandler() {
+    private val logger = LoggerFactory.getLogger(KafkaStoppingErrorHandler::class.java)
+
+    override fun handleRemaining(
+        thrownException: Exception,
+        records: MutableList<ConsumerRecord<*, *>>,
+        consumer: Consumer<*, *>,
+        container: MessageListenerContainer
+    ) {
+        logger.error("En feil oppstod under kafka konsumering av meldinger: \n" + textListingOf(records) +
+                "\nStopper containeren ! Restart er nødvendig for å fortsette konsumering", thrownException)
+        super.handleRemaining(thrownException, records, consumer, container)
+    }
+
+    fun textListingOf(records: List<ConsumerRecord<*, *>>) =
+        records.joinToString(separator = "\n") {
+            "-" .repeat(20) + "\n" + vaskFnr(it.toString())
+        }
+
+    private fun vaskFnr(tekst: String) = tekst.replace(Regex("""\b\d{11}\b"""), "***")
+}
