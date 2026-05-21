3ecec8a62620c2fa6e179afe3828cbdb876c4fc7
mariampervez <mariam.pervez@nav.no>
2026-05-21 07:40:05 +0200

    d - Endrer tid for kjøring

--- src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/KjorFil.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/KjorFil.kt
@@ -17,7 +17,7 @@ class KjorFil (
 ) {
     private val logger: Logger by lazy { LoggerFactory.getLogger(KjorFil::class.java) }
 
-        @Scheduled(cron = "0 26 7 * * *")
+        @Scheduled(cron = "0 45 5 * * *")
     fun lesFilOgLagreTilS3() {
         logger.info("Starter lesing av fil for å legge fnr til S3 ")
         try {
