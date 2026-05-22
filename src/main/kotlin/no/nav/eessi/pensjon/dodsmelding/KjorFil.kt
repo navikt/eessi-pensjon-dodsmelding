bab93f8ca4d57589217c2486bdfbcd93cf98d539
mariampervez <mariam.pervez@nav.no>
2026-05-22 14:16:38 +0200

    t - Kjøres edifact fil

--- src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/KjorFil.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/KjorFil.kt
@@ -17,7 +17,7 @@ class KjorFil (
 ) {
     private val logger: Logger by lazy { LoggerFactory.getLogger(KjorFil::class.java) }
 
-        @Scheduled(cron = "0 8 10 * * *")
+        @Scheduled(cron = "0 22 12 * * *")
     fun lesFilOgLagreTilS3() {
         logger.info("Starter lesing av fil for å legge fnr til S3 ")
         try {
