bfc170b4fcaf835ba989d9a33168c70195f741cf
Daniel Skarpås <dskarpas@gmail.com>
2026-07-03 09:30:02 +0200

    T - Retter test for uthenting av pin; legger til egen liste for pins  (epd)

--- src/test/kotlin/no/nav/eessi/pensjon/dodsmelding/OpprettH070Test.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/dodsmelding/OpprettH070Test.kt
@@ -2,6 +2,7 @@ package no.nav.eessi.pensjon.dodsmelding
 
 import io.mockk.every
 import io.mockk.mockk
+import no.nav.eessi.pensjon.eux.model.sed.PinItem
 import no.nav.eessi.pensjon.h070.OpprettH070
 import no.nav.eessi.pensjon.personoppslag.pdl.model.*
 import no.nav.person.pdl.leesah.Personhendelse
@@ -15,14 +16,16 @@ class OpprettH070Test {
     private val opprettH070 = OpprettH070()
 
     @Test
-    fun `Oppretter H070 og sender denne dersom vi får inn dødsmelding fra Sverige`() {
+    fun `oppretter h070 og sender denne dersom vi far inn dodsmelding fra sverige`() {
+        val identSverige = "SE1234567890"
+        val identNorge = "12345678901"
         val pdlPerson = PdlPerson(
             navn = Navn("Lever", "Ikke", "Lenger", metadata = mockMeta()),
             foedselsdato = Foedselsdato(1950,"1950-10-10", folkeregistermetadata = Folkeregistermetadata(), metadata = mockMeta()),
             kjoenn = Kjoenn(KjoennType.KVINNE, metadata = mockMeta()),
             utenlandskIdentifikasjonsnummer = listOf(
                 UtenlandskIdentifikasjonsnummer(
-                    identifikasjonsnummer = "SE1234567890",
+                    identifikasjonsnummer = identSverige,
                     utstederland = "SWE",
                     opphoert = false,
                     metadata = mockMeta()
@@ -30,7 +33,7 @@ class OpprettH070Test {
             ),
             identer = listOf(
                 IdentInformasjon(
-                    "12345678901",
+                    identNorge,
                     IdentGruppe.FOLKEREGISTERIDENT
                 )
             ),
@@ -42,11 +45,21 @@ class OpprettH070Test {
         )
 
         val personhendelse = mockk<Personhendelse> {
-            every { personidenter } returns listOf("12345678901")
+            every { personidenter } returns listOf(identNorge)
             every { doedsfall } returns no.nav.person.pdl.leesah.doedsfall.Doedsfall(LocalDate.of(2024, 5, 1))
         }
+        val pin = listOf(
+            PinItem(
+                identifikator = identNorge,
+                land = "NOR"
+            ),
+            PinItem(
+                identifikator = identSverige,
+                land = "SWE"
+            )
+        )
 
-        val response = opprettH070.preutFyltH070(personhendelse, pdlPerson)
+        val response = opprettH070.preutFyltH070(personhendelse, pdlPerson, pin)
 
         assertEquals("2024-05-01", response.hnav?.bruker?.doedsfall?.doedsdato)
 
