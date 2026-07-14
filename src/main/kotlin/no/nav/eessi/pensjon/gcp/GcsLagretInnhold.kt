c57aad4f0b290ae9ddac91cc61e6501df875f59a
Daniel Skarpås <dskarpas@gmail.com>
2026-07-14 08:31:00 -0700

    F - Legger til klasse for henting antall personer lagret på gcp

---
+++ src/main/kotlin/no/nav/eessi/pensjon/gcp/GcsLagretInnhold.kt
@@ -0,0 +1,37 @@
+package no.nav.eessi.pensjon.gcp
+
+import org.springframework.beans.factory.annotation.Value
+import com.google.cloud.storage.Storage
+import org.slf4j.LoggerFactory
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+
+class GcpLagretInnhold (
+    private val storage: Storage,
+    @Value("\${GCP_BUCKET_UTL_YTELSE}")
+    private val bucketName: String
+) : ApplicationRunner {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    override fun run(args: ApplicationArguments) {
+        runCatching {
+            storage
+                .list(bucketName)
+                .iterateAll()
+                .count()
+        }.onSuccess { count ->
+            logger.info(
+                "Bucket {} har {} personer",
+                bucketName,
+                count
+            )
+        }.onFailure { error ->
+            logger.error(
+                "Error under henting av info fra $bucketName {}",
+                bucketName,
+                error
+            )
+        }
+    }
+}
