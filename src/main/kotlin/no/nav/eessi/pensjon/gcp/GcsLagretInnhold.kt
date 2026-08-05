cf5a3810e61b1d51f7fc37479b1d1538a8af6a60
Daniel Skarpås <dskarpas@gmail.com>
2026-08-05 08:57:42 +0200

    F - Endrer logging for antallet lagret  (epd)

--- src/main/kotlin/no/nav/eessi/pensjon/gcp/GcsLagretInnhold.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/gcp/GcsLagretInnhold.kt
@@ -24,17 +24,9 @@ class GcpLagretInnhold (
                 .iterateAll()
                 .count()
         }.onSuccess { count ->
-            logger.info(
-                """
-                ************************************************************
-                GCP bucket summary:
-                - bucket: {}
-                - antall lagrede personer: {}
-                ************************************************************
-                """.trimIndent(),
-                bucketName,
-                count
-            )
+            logger.info("************************************************************")
+            logger.info("GCP bucket summary: bucket={}, antall lagrede personer={}", bucketName, count)
+            logger.info("************************************************************")
         }.onFailure { error ->
             logger.error(
                 "Error under henting av info fra $bucketName {}",
