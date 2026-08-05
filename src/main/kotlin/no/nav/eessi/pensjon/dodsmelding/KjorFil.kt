b0e226d530904111ec64804a3f7eb26dd53728d5
Daniel Skarpås <dskarpas@gmail.com>
2026-08-05 09:04:41 +0200

    F - Kjører henting av edifact  (epd)

--- src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/KjorFil.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/KjorFil.kt
@@ -12,12 +12,12 @@ import org.springframework.stereotype.Service
 @Service
 @EnableScheduling
 class KjorFil (
-//    @param:Value("\${GCP_BUCKET_UTL_YTELSE}") var utenlandkYtelseBucket: String,
+    @param:Value("\${GCP_BUCKET_UTL_YTELSE}") var utenlandkYtelseBucket: String,
     private val lagringsService: LagringsService
 ) {
     private val logger: Logger by lazy { LoggerFactory.getLogger(KjorFil::class.java) }
 
-        @Scheduled(cron = "0 30 13 * * *")
+        @Scheduled(cron = "0 15 09 * * *")
     fun lesFilOgLagreTilS3() {
         logger.info("Starter lesing av fil for å legge fnr til S3 ")
         try {
