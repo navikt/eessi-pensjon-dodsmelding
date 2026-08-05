fcbc611a581b09b94acb02422efcf241b5198b7f
Daniel Skarpås <dskarpas@gmail.com>
2026-08-05 15:12:19 +0200

    F - Kjører batch innlesing av filer  (epd)

--- src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/KjorFil.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/KjorFil.kt
@@ -17,7 +17,7 @@ class KjorFil (
 ) {
     private val logger: Logger by lazy { LoggerFactory.getLogger(KjorFil::class.java) }
 
-        @Scheduled(cron = "0 05 08 * * *")
+        @Scheduled(cron = "0 20 13 * * *")
     fun lesFilOgLagreTilS3() {
         logger.info("Starter lesing av fil for å legge fnr til S3 ")
         try {
