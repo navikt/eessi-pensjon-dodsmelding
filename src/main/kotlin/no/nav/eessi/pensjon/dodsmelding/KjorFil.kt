6b0d214d43093dc0146df6bfbed4dd0eafa9eefa
Daniel Skarpås <dskarpas@gmail.com>
2026-08-06 12:23:51 +0200

    R - Ny kjøring  (epd)

--- src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/KjorFil.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/KjorFil.kt
@@ -17,7 +17,7 @@ class KjorFil (
 ) {
     private val logger: Logger by lazy { LoggerFactory.getLogger(KjorFil::class.java) }
 
-        @Scheduled(cron = "0 23 10 * * *")
+        @Scheduled(cron = "0 30 10 * * *")
     fun lesFilOgLagreTilS3() {
         logger.info("Starter lesing av fil for å legge fnr til S3 ")
         try {
