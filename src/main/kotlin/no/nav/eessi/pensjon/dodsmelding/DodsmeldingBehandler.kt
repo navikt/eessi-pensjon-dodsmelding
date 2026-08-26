1c739ab1d68a09207720d27e35a260f94b7977db
Daniel Skarpås <dskarpas@gmail.com>
2026-08-26 09:03:02 +0200

    R - Secure logging av oppholdsadresse  (epd)

--- src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/DodsmeldingBehandler.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/DodsmeldingBehandler.kt
@@ -64,7 +64,8 @@ class DodsmeldingBehandler(
             logger.info("Bruker har utenlandsk adresse. Oppretter dermed ikke H070")
             return
         }
-        secureLogger.info("Personhendelse for H070: ${person.bostedsadresse?.toJson()}")
+        secureLogger.info("bostedsadresse for H070: ${person.bostedsadresse?.toJson()}")
+        secureLogger.info("oppholdsadresse for H070: ${person.oppholdsadresse?.toJson()}")
 
         val pin = opprettPinListe(person)
         if (pin.none { it.land == "NOR" }) {
