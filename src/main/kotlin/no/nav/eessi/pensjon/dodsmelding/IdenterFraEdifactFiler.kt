8c3c89b1efff913b13ca4653775083f47aa53709
Daniel Skarpås <dskarpas@gmail.com>
2026-09-24 10:01:41 +0200

    F - Ny kjøring av edifact fil (Sverige  511)  (epd)

--- src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/IdenterFraEdifactFiler.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/IdenterFraEdifactFiler.kt
@@ -22,7 +22,7 @@ class IdenterFraEdifactFiler (
 ) {
     private val logger: Logger by lazy { LoggerFactory.getLogger(IdenterFraEdifactFiler::class.java) }
 
-        @Scheduled(cron = "0 53 07 * * *")
+        @Scheduled(cron = "0 10 08 * * *")
     fun hentIdenterFraEdifactBatch() {
         logger.info("Starter lesing av fil for å legge fnr til S3 ")
         try {
