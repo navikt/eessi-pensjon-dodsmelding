6f2162387b360a938246eef5c00842308dffd7a6
Daniel Skarpås <dskarpas@gmail.com>
2026-09-24 10:40:00 +0200

    F - Utvider edifactdok med svenskIdent  (epd)

--- src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/EdifactDokument.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/dodsmelding/EdifactDokument.kt
@@ -1,13 +1,14 @@
 package no.nav.eessi.pensjon.dodsmelding
 
 open class EdifactDokument(
-    val avsender: String?,
-    val mottaker: String?,
-    val meldingstype: String?,
-    val norskIdent: String?,
-    val avsenderLand: String?,
-    val mottakerLand: String?,
-    val fodselsdato: String?,
+    val avsender: String? = null,
+    val mottaker: String? = null,
+    val meldingstype: String? = null,
+    val norskIdent: String? = null,
+    val svenskIdent: String? = null,
+    val avsenderLand: String? = null,
+    val mottakerLand: String? = null,
+    val fodselsdato: String? = null,
     val erSveFin: Boolean,
     val doedsdato: String? = null
 )
